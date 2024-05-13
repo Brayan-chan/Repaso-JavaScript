bb5cdc04f9f0aefa980466bd5d287d6e69f955a7
Brayan-chan <al071392@uacam.mx>
2024-05-12 18:44:40 -0600

    personalice alerts for searching successful and error searching

--- buscador.js
+++ buscador.js
@@ -10,13 +10,16 @@ var infoDiv = document.querySelector('.info');
 var buscadorInput = document.getElementById('buscador');
 
 // Agregar el evento input al campo de búsqueda
-buscadorInput.addEventListener('input', function() {
+buscadorInput.addEventListener('input', function() { // el evento input se activa cuando el usuario cambia el valor del campo de búsqueda
     // Obtener el valor del campo de búsqueda
     var terminoBusqueda = this.value.toLowerCase();
     // Verificar si el tema buscado está en el objeto 'temas'
-    if (temas.hasOwnProperty(terminoBusqueda)) {
+    if (temas.hasOwnProperty(terminoBusqueda)) { // hasOwnProperty verifica si un objeto tiene una propiedad
         // Si está, insertar la información relevante en el div info
-        infoDiv.textContent = temas[terminoBusqueda];
+        infoDiv.textContent = temas[terminoBusqueda]; // el objeto temas es un objeto que contiene la información relevante para cada tema
+    } else if (terminoBusqueda === '') { // si el campo de búsqueda está vacío
+        // Si no está, mostrar un mensaje de error
+        infoDiv.textContent = '';
     } else {
         // Si no está, mostrar un mensaje de error
         infoDiv.textContent = "No se encontró información para '" + terminoBusqueda + "'";
